8a9a53b8509695d7cf3c79af91424f120879e0a5
Serge Gron <serge.gron@gmail.com>
2023-06-26 00:16:22 +0300

    Added TOOLS directory with build script

---
+++ TOOLS/win-qt6-msvc17-64-debug.bat
@@ -0,0 +1,25 @@
+﻿@echo off
+
+set QTVER=6
+set BUILDER=msvc17
+set TARGET_PLATFORM=win32
+set SIZE=64
+set GENERATOR="Visual Studio 17 2022"
+set ENV_PATH="c:/Qt-6.3.0"
+set CONFIGURATION="debug"
+
+::----------------------------
+:: DO NOT EDIT THE TEXT BELOW!
+::----------------------------
+
+set QT=qt%QTVER%
+set TARGET=%TARGET_PLATFORM%-%QT%-%BUILDER%-%SIZE%-%CONFIGURATION%
+
+mkdir BUILD
+cd BUILD
+mkdir %TARGET%
+cd %TARGET%
+
+cmake -DQT=%QTVER% -DTARGET_DIR=%TARGET% -DTARGET_PLATFORM=%TARGET_PLATFORM% -DENV_PATH=%ENV_PATH% -DCONFIGURATION=%CONFIGURATION% -G %GENERATOR% ../..
+
+cmake --build . --config %CONFIGURATION%
